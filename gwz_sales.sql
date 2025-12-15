8ad427a0665881c7af434ed1ebe9dc92bd3f89e1
Meltem-93 <meltem.iscan93@gmail.com>
2025-12-15 13:47:20 +0300

    add_purchase_cost

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,7 +1,10 @@
 SELECT
+ SELECT
      date_date
  , SUM(turnover) AS turnover
+     , SUM(turnover) AS turnover
+     , SUM(purchase_cost) AS purchase_cost
  FROM `course14.gwz_sales`
  GROUP BY date_date
  ORDER BY date_date;
- 
+  ORDER BY date_date;
