0b3aae6ff8102abc34526076b437a5c176656a5a
Meltem-93 <meltem.iscan93@gmail.com>
2025-12-16 10:45:40 +0300

    Most recent dates first

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -7,4 +7,9 @@ SELECT
  FROM `course14.gwz_sales`
  GROUP BY date_date
  ORDER BY date_date;
-  ORDER BY date_date;
+  ORDER BY date_date;date_date
+, ROUND (SUM(turnover), 2) AS turnover
+, ROUND(SUM(purchase_cost), 2) AS purchase_cost
+FROM 'course14.gwz_sales
+GROUP BY date_date
+ORDER by date_date DESC'
